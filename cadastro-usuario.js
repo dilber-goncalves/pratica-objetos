8258f3305fc72a26fea6997943a8d08b5d576f1a
Dilber Goncalves <dilberkeybd@gmail.com>
2024-04-02 18:54:12 -0300

    02-04

---
+++ cadastro-usuario.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: 'Rua Dois de Julho',
+    numero: '286',
+    complemento: 'apto 202',
+    bairro: 'Centro',
+    cep: '41.000-000',
+    cidade: 'Salvador',
+    estado: 'Bahia',
+    pais: 'Brasil',
+}
+
+const usuario = {
+    nome: 'Guido Siqueira',
+    data_nascimento: '02-02-2002',
+    email: 'guido@email.com',
+    telefone: '(00) 99999-8888',
+    endereco
+}
+
+console.log(usuario);
---
+++ lista-veiculos.js
@@ -0,0 +1,28 @@
+const carros = [
+    {
+        marca:'Ford',
+        modelo: 'Ka',
+        ano: 2020,
+        cor: 'Branco',
+        quantidade_portas: 4,
+        automatico: false
+    },
+    {
+        marca:'Toyota',
+        modelo: 'Corolla',
+        ano: 2021,
+        cor: 'Prata',
+        quantidade_portas: 4,
+        automatico: true
+    },
+    {
+        marca:'Fiat',
+        modelo: 'Uno',
+        ano: 2004,
+        cor: 'Vermelho',
+        quantidade_portas: 2,
+        automatico: false
+    }
+]
+
+console.log(carros);
